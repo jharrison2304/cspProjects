0569ca67c1f9d306a73349c262f471fb1fa110c7
Joshua Harrison <j.harrison@students.chapelfield.org>
2019-02-21 12:36:21 -0500

    Comment blink

--- arduino/blink/blink.ino
+++ arduino/blink/blink.ino
@@ -5,14 +5,14 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  delay(2000);
-  digitalWrite(13, HIGH);
-  delay(500);
-  digitalWrite(13, LOW);
-  delay(500);
-  digitalWrite(13, HIGH);
-  delay(500);
+  digitalWrite(13, HIGH); //tells pin 13 to be on
+  delay(1000); //tells the program to wait 1 second
+  digitalWrite(13, LOW); //tells pin 13 to be off
+  delay(2000); //tells program to wait 2 seconds
+  digitalWrite(13, HIGH); //tells pin 13 to be on
+  delay(500); //tells program to wait 5oo milliseconds
+  digitalWrite(13, LOW); //tells pin 13 to be off
+  delay(500); //tells program to wait 500 milliseconds
+  digitalWrite(13, HIGH); //tells pin 13 to be on
+  delay(500); //tells program to wait 500 milliseconds
 }
