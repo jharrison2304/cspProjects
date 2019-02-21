d0cc565aa80d490f89a5314c65de4a703bc66558
Joshua Harrison <j.harrison@students.chapelfield.org>
2019-02-21 14:56:22 -0500

    Make external light blink

--- arduino/blink/blink.ino
+++ arduino/blink/blink.ino
@@ -1,18 +1,18 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH); //tells pin 13 to be on
+  digitalWrite(12, HIGH); //tells pin 13 to be on
   delay(1000); //tells the program to wait 1 second
-  digitalWrite(13, LOW); //tells pin 13 to be off
+  digitalWrite(12, LOW); //tells pin 13 to be off
   delay(2000); //tells program to wait 2 seconds
-  digitalWrite(13, HIGH); //tells pin 13 to be on
+  digitalWrite(12, HIGH); //tells pin 13 to be on
   delay(500); //tells program to wait 5oo milliseconds
-  digitalWrite(13, LOW); //tells pin 13 to be off
+  digitalWrite(12, LOW); //tells pin 13 to be off
   delay(500); //tells program to wait 500 milliseconds
-  digitalWrite(13, HIGH); //tells pin 13 to be on
+  digitalWrite(12, HIGH); //tells pin 13 to be on
   delay(500); //tells program to wait 500 milliseconds
 }
