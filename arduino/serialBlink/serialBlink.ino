64badec4f75c213fb0914e0996591f386fb0b593
Joshua Harrison <j.harrison@students.chapelfield.org>
2019-03-07 12:00:42 -0500

    Add additional Serial.println to setup

--- arduino/serialBlink/serialBlink.ino
+++ arduino/serialBlink/serialBlink.ino
@@ -1,7 +1,9 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println("smile because smiles are like similes except without the extra i")
+  Serial.println("smile because smiles are like similes except without the extra i");
+  Serial.println("Send 'On' tp turn on the LED.");
+  Serial.println("Send 'Off' to turn off the LED.");
   pinMode(13, OUTPUT);
 }
 
