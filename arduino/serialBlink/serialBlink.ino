d364d48d298ee96917b4bdd19ce7e134f715e494
Joshua Harrison <j.harrison@students.chapelfield.org>
2019-03-12 12:04:25 -0400

    Update serialBlink with Serial.available

--- arduino/serialBlink/serialBlink.ino
+++ arduino/serialBlink/serialBlink.ino
@@ -1,24 +1,27 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  Serial.setTimeout(10);
   Serial.println("smile because smiles are like similes except without the extra i");
-  Serial.println("Send 'On' tp turn on the LED.");
+  Serial.println("Send 'On' to turn on the LED.");
   Serial.println("Send 'Off' to turn off the LED.");
   pinMode(13, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (Serial.readString() == "On") {
-    digitalWrite(13, HIGH);
+  if (Serial.available() > 0) {
+    String command;
+    command = Serial.readString();
+    command.trim();
+    if (command == "On") {
+      digitalWrite(13, HIGH);
+      Serial.println("On");
+    }
+    else if (command == "Off") {
+      digitalWrite(13, LOW);
+      Serial.println("Off");
+    }
+
   }
-  else if (Serial.readString() == "Off") {
-    digitalWrite(13, LOW);
-  }
-  digitalWrite(13, HIGH);
-  Serial.println("On");
-  delay(1000);
-  digitalWrite(13, LOW);
-  Serial.println("Off");
-  delay(1000);
 }
+// put your main code here, to run repeatedly:
