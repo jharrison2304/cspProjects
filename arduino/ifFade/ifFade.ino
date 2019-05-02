7ad627813f45fcf2de3ab4d98a998578bdfce77b
Joshua Harrison <j.harrison@students.chapelfield.org>
2019-05-02 11:55:44 -0400

    Add if statement and lightValue code to ifFade.

---
+++ arduino/ifFade/ifFade.ino
@@ -0,0 +1,16 @@
+int lightValue = 0;
+int delta = 1;
+void setup() {
+  Serial.begin(9600);
+  pinMode(9, OUTPUT);
+
+
+}
+
+void loop() {
+  lightValue = lightValue + delta;
+  if (lightValue <= 0 || lightValue >= 255) {
+    delta = -delta;
+  }
+
+}
