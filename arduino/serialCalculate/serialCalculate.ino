a46b3c2ad5fe563886889207bcc0b68603e1250f
Joshua Harrison <j.harrison@students.chapelfield.org>
2019-05-16 12:14:51 -0400

    Create variable cleanData and clean up the serial monitor output

--- arduino/serialCalculate/serialCalculate.ino
+++ arduino/serialCalculate/serialCalculate.ino
@@ -1,10 +1,21 @@
+int number1;
+char sign;
+int number2;
+int result;
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin()
+  Serial.begin(9600);
+
 
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  cleanData();
+}
+void cleanData() {
+  while (Serial.available() > 0) {
+    char inbound = Serial.read();
+    if (isGraph(inbound)) {
+      Serial.println(inbound);
+    }
+  }
 }
