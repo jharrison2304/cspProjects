5f20e1b1b087621dbc02d5e2812669e25e7efc25
Joshua Harrison <j.harrison@students.chapelfield.org>
2019-06-03 14:50:34 -0400

    change array to integer

--- arduino/serialCalculate/serialCalculate.ino
+++ arduino/serialCalculate/serialCalculate.ino
@@ -2,20 +2,32 @@ int number1;
 char sign;
 int number2;
 int result;
+char cleanData[32];
+bool newData = false;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-
-
 }
 void loop() {
-  cleanData();
+  cleanData1();
 }
-void cleanData() {
+void cleanData1() {
+  int count = 0;
   while (Serial.available() > 0) {
     char inbound = Serial.read();
     if (isGraph(inbound)) {
-      Serial.println(inbound);
+      newData = true;
+      cleanData[count] = inbound;
+      count++;
+      Serial.println(cleanData);
     }
+  }
+}
+
+void printCleanData() {
+  if (newData == true) {
+    Serial.println(cleanData);
+    char cleanData[32] = "";
+    newData = false;
   }
 }
